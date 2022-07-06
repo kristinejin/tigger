08354ddc1fca5e9d4e33d65fe7614c8e8a277d0e
YITONG JIN <z5362038@ad.unsw.edu.au>
2022-07-06 17:20:15 +1000

    implemented log

---
+++ .tigger/branches/master/repo/demo.sh
@@ -0,0 +1,17 @@
+#! /bin/dash
+
+# if [ -f ".tigger/HEAD" ]
+# then
+#     echo "This file exists on your filesystem."
+# fi
+
+# hello
+ 
+
+ 
+# index=.tigger/branches/master/index
+# for f in $index/*
+# do 
+#     echo $f
+#     echo 1
+# done
